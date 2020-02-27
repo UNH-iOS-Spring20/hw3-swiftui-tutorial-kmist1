ef4922aea85de1b4bdf89446ce83ddd8ecfcd38b
Krunal <kmist1@unh.newhaven.edu>
2020-02-27 18:38:08 -0500

    Landmark

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,7 +10,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            MapView()
+            .edgesIgnoringSafeArea(.top)
+            .frame(height: 300)
+            
+             CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            
+            VStack(alignment: .leading) {
+                VStack {
+                    VStack {
+                        Text("Turtle Rock")
+                            .font(.title)
+                    }
+                        
+                }
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+        
     }
 }
 
